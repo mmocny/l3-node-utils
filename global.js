d040daeda03c83852cf2da59c12b87f5160d0faa
Michal Mocny <mmocny@gmail.com>
2015-02-26 15:54:26 -0500

    6to5 -> babel and start using co()

--- global.js
+++ global.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require("babel/register");
+
 // TODO: can we replace this with something that is less hacky?
 var utils = require(require('./package.json').main);
 for (var key in utils)
